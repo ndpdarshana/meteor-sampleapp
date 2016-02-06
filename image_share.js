9e2360ff46b08e74929bed1933753edc93100413
ndpdarshana <ndp.mymail@gmail.com>
2016-02-07 00:46:29 +0530

    Use data array in template helper

--- image_share.js
+++ image_share.js
@@ -1,9 +1,19 @@
 if (Meteor.isClient) {
-  var img_data = {
-    img_src:"image1.jpg",
-    img_alt:"Sew Hele image I added"
-  }
-  Template.images.helpers(img_data);
+  var img_data = [
+    {
+      img_src:"image1.jpg",
+      img_alt:"Sew Hele image I added"
+    },
+    {
+      img_src:"image2.jpg",
+      img_alt:"Kala wewa"
+    },
+    {
+      img_src:"image3.jpg",
+      img_alt:"Awukana"
+    },
+  ]
+  Template.images.helpers({images:img_data});
 }
 
 if (Meteor.isServer) {
